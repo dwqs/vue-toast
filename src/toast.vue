af971487d6fccc356ca564d1a181d202236e9696
dwqs <461147874@qq.com>
2016-11-23 18:04:06 +0800

    v0.5.2

--- src/toast.vue
+++ src/toast.vue
@@ -1,6 +1,8 @@
 <template>
     <div v-show="shown" class="toast-mask">
-        <div class="test">{{message}}</div>
+        <div class="toast">
+            {{message}}
+        </div>
     </div>
 </template>
 
@@ -12,8 +14,35 @@
         right: 0;
         bottom: 0;
         left: 0;
+        font-size: 16px;
         background-color: transparent;
         box-sizing: border-box;
+        *{
+            box-sizing: border-box;
+        }
+    }
+
+    .toast{
+        position: absolute;
+        top: 24px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        width: 5em;
+        height: 3em;
+        font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+        font-size: 16px;
+        color: #fff;
+        text-align: center;
+        border-radius: 4px;
+        background: rgba(0, 0, 0, 0.7);
+        &:before{
+            content: '';
+            display: inline-block;
+            width: 0;
+            height: 100%;
+            font-size: 0;
+            vertical-align: middle;
+        }
     }
 </style>
 
