d40ee538616655962f43e0333b21d3b6dfcf1ccf
dwqs <461147874@qq.com>
2016-11-23 16:39:01 +0800

    project layout

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+/**
+ * Created by pomy on 16/4/7.
+ */
+
+var path = require('path');
+
+var extensions = ['','.js','.jsx','.vue'];
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'),
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './lib'),
+        publicPath: path.resolve(__dirname, '/lib/')
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/ 
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            },
+            {
+                test:/\.css$/,
+                loader:'vue-style!css'
+            },
+            {
+                test: /\.less$/,
+                loader: 'vue-style!css!less'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: extensions
+    }
+};
